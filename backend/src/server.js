b768fb31af5e2eeef0936ee05d8ec635e7fe7c89
Naty-Mulgeta <fikirlehulum710@gmail.com>
2026-04-23 07:05:39 -0900

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv"
 import dns from "node:dns/promises"
 import rateLimiter from "./middleware/rateLimiter.js"
 import cors from "cors"
+import path from "path"
 
 dns.setServers([
     '1.1.1.1',
@@ -15,10 +16,14 @@ dotenv.config()
 
 const app=express()
 const PORT=process.env.PORT || 5001 //we put 5001 as fallback value incase PORT didn't work
+const __dirname=path.resolve()
 
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
     origin:"http://localhost:5173",
 }))
+}
+
  
 app.use(express.json()) //to access title and Description in notesController //we send them as a json //to get access to the req.body 
 
@@ -26,6 +31,14 @@ app.use(rateLimiter)
 
 
 app.use("/api/notes",notesRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
